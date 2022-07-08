3ea415bcd74ef34338c4efa7bea833d30765839c
Vytautas <vyckelis@gmail.com>
2022-07-08 14:05:09 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log(`main.js`);
+console.log('main.js');
+
+let sum = 0;
+// debugger;
+for (let i = 0; i < 6; i++) {
+  sum += i;
+  // console.log(`sis ciklas yra ${i}. bendra suma = ${sum} `);
+}
+// console.log(`Bendra suma po visko = ${sum} `);
+// debugger;
+if (sum === 15) {
+  console.log('penkiolika');
+} else {
+  console.log('nepenkiolika');
+}
+
+console.group('continue soka tiesiai i i++');
+// atspausdinti skaicius nuo 10 iki 20 bet praleisti 15
+for (let i = 10; i <= 20; i++) {
+  if (i === 15) {
+    debugger;
+    // continue soka tiesiai i i++
+    // continue;
+    break;
+  }
+  console.log(`skaicius ${i}`);
+}
+console.groupEnd();
